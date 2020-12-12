ec14e3239dd14a0edf22d66aaf28d7c284f3c465
zulqarnain-99 <zulqi99@gmail.com>
2020-12-12 10:23:51 +0500

    add list in dropdown list

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Retrieving_select_item_from_fropdownlist_Asp.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Retrieving_select_item_from_fropdownlist_Asp.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42ef25ae-cb3e-4bc9-adaa-3d035a5ababb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Retrieving_select_item_from_fropdownlist_Asp.Net.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem Text="Select City" Value="-1"></asp:ListItem>
+                <asp:ListItem Text="Karachi" Value="1"></asp:ListItem>
+                <asp:ListItem Text="Lahore" Value="2"></asp:ListItem>
+                <asp:ListItem Text="Faislabad" Value="3"></asp:ListItem>
+                <asp:ListItem Text="Mumbai" Value="4"></asp:ListItem>
+                <asp:ListItem Text="Delhi" Value="5"></asp:ListItem>
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
